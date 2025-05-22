d011e5bbd2e86df06c033c49efb72a0b577de412
Sean Imhof <imhofsea@students.zhaw.ch>
2025-05-22 21:27:48 +0200

    correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://seanimhof.github.io/epd-frontend/dist/',
+  base: '/epd-frontend/',
   plugins: [
     vue(),
     vueDevTools(),
